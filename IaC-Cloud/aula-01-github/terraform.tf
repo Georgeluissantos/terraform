077fac71ce54e8c72d036c81da7de104bb6778a6
George-Luis <cloudgeorge50@gmail.com>
2024-10-03 22:52:02 -0300

    Add: terraform e Provider

---
+++ IaC-Cloud/aula-01-github/terraform.tf
@@ -0,0 +1,14 @@
+terraform { 
+  required_providers { 
+    aws = { 
+      source = "hashicorp/aws" 
+      version = "5.69.0" 
+    } 
+
+       github = { 
+      source = "integrations/github" 
+      version = "6.3.0" 
+
+  } 
+} 
+ 
