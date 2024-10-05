b3eb7667f09621c2eb4df910ac9934b2be166c3b
George-Luis <cloudgeorge50@gmail.com>
2024-10-05 16:59:35 -0300

    Add.: recurso ref. a subnet

---
+++ 01-IaC-CloudNetwork/aws_subnet.tf
@@ -0,0 +1,16 @@
+#Criação de Subnet Public e Private
+SubNet - 1a
+resource "aws_subnet" "private_subnet_us_east_1a" {
+  vpc_id            = aws_vpc.prod_vpc.id 
+  cidr_block        = var.private_cidr_block_us_east_1a 
+  availability_zone = var.availability_zone_us_east_1a 
+  tags              = var.private_subnet_us_east_1a_tag 
+}
+
+resource "aws_subnet" "public_subnet_us_east_1a" {
+  vpc_id            = aws_vpc.prod_vpc.id 
+  cidr_block        = var.public_cidr_block_us_east_1a 
+  availability_zone = var.availability_zone_us_east_1a
+  tags              = var.public_subnet_us_east_1a_tag 
+
+}
